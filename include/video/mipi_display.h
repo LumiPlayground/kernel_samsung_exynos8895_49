5e1b2372426445ed76d14d7891b3491608ed8b12
Nguyễn Long <dmgogle123@gmail.com>
2024-06-10 10:23:01 +0000

    include: mipi_display: Add missing pieces
    Change-Id: I0e10f63e2d9d0e77c11d94e6723eeec9e00aeb81

--- include/video/mipi_display.h
+++ include/video/mipi_display.h
@@ -62,6 +62,8 @@ enum {
 
 	MIPI_DSI_DSC_PRA				= 0x07,
 	MIPI_DSI_DSC_PPS				= 0x0a,
+	MIPI_DSI_OEM1_WR_SIDE_RAM		= 0x4C,
+	MIPI_DSI_OEM1_WR_SIDE_RAM2		= 0x5C,
 };
 
 /* MIPI DSI Peripheral-to-Processor transaction types */
